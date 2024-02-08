de588226b8dc96b18843217e2bbf9959e7be5763
Urassamx7 <marvin.lagas01234@gmail.com>
2024-02-08 22:19:46 +0200

    The last one ;)

--- src/app.tsx
+++ src/app.tsx
@@ -3,25 +3,25 @@ import { NoteCard } from './components/note-card'
 import { NewnoteCard } from './components/new-noteCard'
 import { ChangeEvent, useState } from 'react'
 
-interface Note {
+interface Note {//Propriedades gerais que todas as notas Vão possuir
   id: string
   date: Date
   content: string
 }
 export function App() {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState('')// Estado inicial que uma pesquisa irá conter[Usado na linha 50]
 
   const [notes, setNotes] = useState<Note[]>(() => {
     //função para Busca de notas já existentes no localStorage
     const notesOnStorage = localStorage.getItem('notes') //Pegando as notas
     if (notesOnStorage) { //Se existem faça:
       return JSON.parse(notesOnStorage) //Retorna as notas que foram transformadas em JSON na linha 29
-    } else {
+    } else {//Se não:
       return [];
     }
   })
 
-  function onNoteCreated(content: string) {
+  function onNoteCreated(content: string) {//É passada por paramêtro para a componente que irá receber o content da Nota <NewNoteCard/>
     const newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
@@ -29,7 +29,7 @@ export function App() {
     } //Objecto que contém as propriedades gerais de uma nota!!
 
     const notesArray = [newNote, ...notes]//Array que contém as notas
-    setNotes(notesArray) //Armazenando as notas na var = notes, para fácil transformação de Array para JSON
+    setNotes(notesArray) //Armazenando as notas na var => notesArray, para fácil transformação de Array para JSON
     localStorage.setItem('notes', JSON.stringify(notesArray)) //Salvar no local Storage do Browser
 
   }
@@ -45,9 +45,8 @@ export function App() {
     const query = event.target.value
     setSearch(query)
   }
-
-
-  const fillteredNotes = search !== '' ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase())) : notes //Filtros para pesquisa de notas
+  const filteredNotes = search !== '' ? notes.filter((note) => note.content.toLowerCase().includes(search.toLowerCase())) : notes 
+  //Filtros para pesquisa de notas
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 p-5 md:px-0">
@@ -61,13 +60,12 @@ export function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewnoteCard onNoteCreated={onNoteCreated} />
         {
-          fillteredNotes.map((note) => {
+          filteredNotes.map((note) => {
             return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
           }
-
           )
         }
 
